21c6898b5003fdd3522a92ccd7d915ed342f8927
ff0000 ff <fmx0209@gmail.com>
2019-11-05 15:17:30 +0300

    Non-working version. Problem with sqLite

---
+++ main.go
@@ -0,0 +1,68 @@
+/*              Sentry Bot                */ 
+/*   Protect your real telegramm account  */
+
+package main
+
+import (
+	"fmt"
+	tg "github.com/go-telegram-bot-api/telegram-bot-api" //for compact
+)
+
+type Bot struct {
+	tgApi *tg.BotAPI
+}
+
+const botToken = ""     // YOUR TELEGRAM BOT TOCKEN; SEE @BotFather 
+const ownerID = 00000000// YOUR ACCOUNT ID; SEE @userinfobot 
+                // (dont confuse with same adress crypto chanell)
+
+func main() {
+
+	InitDb() // SQLite
+	bot, err := tg.NewBotAPI(botToken)
+	if err != nil {
+		fmt.Println(err)
+	}
+	msgHub := Bot{
+		tgApi: bot,
+	}
+
+	bot.Debug = false
+	fmt.Printf("Authorized on account %s\n", bot.Self.UserName)
+	u := tg.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+		msgHub.msgHub(update.Message)
+	}
+}
+
+func (Hub *Bot) msgHub(msg *tg.Message) {
+
+	if msg.From.ID != ownerID { //Send msg to owner
+		msgToOwn := tg.NewForward(int64(ownerID), msg.Chat.ID, msg.MessageID)
+		msgInBot, _ := Hub.tgApi.Send(msgToOwn)
+		/*go */ SaveToDb(msgInBot.MessageID, msg.Chat.ID) // Database write
+
+	} else { //Recive msg from owner
+
+		if msg.ReplyToMessage == nil { // quote is reqired
+			missReply := tg.NewMessage(int64(ownerID), "Пропущено цитирование сообщения") //en: missing quote for message
+			missReply.ReplyToMessageID = msg.MessageID
+			Hub.tgApi.Send(missReply)
+			return
+		}
+		searchResult := SearchInDb(msg.ReplyToMessage.MessageID)
+		if searchResult == int64(ownerID) {
+			missMsg := tg.NewMessage(int64(ownerID), "Не найден автор сообщения")
+			missMsg.ReplyToMessageID = msg.MessageID
+			Hub.tgApi.Send(missMsg)
+			return
+		}
+		msgFromOwn := tg.NewMessage(SearchInDb(msg.ReplyToMessage.MessageID), msg.Text) //Read usrID in db
+		Hub.tgApi.Send(msgFromOwn)
+	}
+}
---
+++ sqlitedb.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	//"sync" // experiments with mutex - failed
+	_ "github.com/mattn/go-sqlite3"
+)
+
+
+var database *sql.DB
+
+func InitDb() {
+	var err error
+	database, err = sql.Open("sqlite3", "./idStorage.db")
+	// defer database.Close() - segfault when uncomm... idk wtf
+	errorPrint("Database open file error:", err)
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS msg2usr (msgId INTEGER PRIMARY KEY, usrId INTEGER)")
+	errorPrint("Database prepare, if file db not exist error:", err)
+	_, err = statement.Exec()
+	errorPrint("Database prepare Exec:", err)
+}
+
+func SaveToDb(msgId int, usrId int64) { // Problem HERE
+	// var mutex sync.Mutex
+	// mutex.Lock()
+	
+	statement, err := database.Prepare("INSERT INTO msg2usr (msgId, usrId) VALUES (?, ?)")
+	errorPrint("SaveToDb prepare:", err)
+	_, err = statement.Exec(msgId, usrId)
+	errorPrint("SaveToDb Exec:", err)
+	
+	/*mutex.Unlock()*/
+}
+
+func SearchInDb(targetId int) int64 {
+	rows, err := database.Query("SELECT msgId, usrId FROM msg2usr")
+	errorPrint("SearchInDb() Query:", err)
+
+	var msgId int
+	var usrId int64
+
+	for rows.Next() {
+		err = rows.Scan(&msgId, &usrId)
+		errorPrint("Loop, rows.Scan", err)
+		if msgId == targetId {
+			return usrId
+		}
+	}
+	return ownerID
+}
+
+func errorPrint(comment string, err error) {
+	if err != nil {
+		fmt.Println(comment, err)
+	}
+}
